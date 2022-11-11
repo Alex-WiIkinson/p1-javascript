e0ce4267730df0317c7ef7fb7705ee379a35fcd0
Alex-WiIkinson <9014592@student.zadkine.nl>
2022-11-11 14:16:48 +0100

    +6.4

---
+++ Week6/6.4/app.js
@@ -0,0 +1,23 @@
+// Oefening 1
+function calculate(num1, num2) {
+    let sum = num1 + num2;
+    return sum;
+}
+
+console.log(calculate(1, 12));
+
+// Oefening 2
+function multiplyByFive(num) {
+    let ans = num * 5;
+    return ans;
+}
+
+console.log(multiplyByFive(7));
+
+// Oefening 3
+function calculateMinutes(sec) {
+    let result = sec / 60;
+    return result;
+}
+
+console.log(calculateMinutes(3600));
